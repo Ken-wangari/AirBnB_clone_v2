a221b66e8e333a442f4fee9ae1b967682ea5b147
Ken-wangari <wangariken50@gmail.com>
2024-03-23 13:23:36 -0700

    AirBnB_clone_v2

--- models/user.py
+++ models/user.py
@@ -1,27 +1,18 @@
 #!/usr/bin/python3
 """This is the user class"""
-from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-from models.place import Place
-from models.review import Review
-
+from models.base_model import BaseModel, Base
 
 class User(BaseModel, Base):
-    """This is the class for user
-    Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
-    """
+    """Class representing a user"""
     __tablename__ = "users"
-    email = Column(String(128), nullable=False)
+
+    email = Column(String(128), nullable=False, unique=True)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
-    places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="user")
-    reviews = relationship("Review", cascade='all, delete, delete-orphan',
-                           backref="user")
+
+    places = relationship("Place", cascade="all, delete, delete-orphan", backref="user")
+    reviews = relationship("Review", cascade="all, delete, delete-orphan", backref="user")
+
