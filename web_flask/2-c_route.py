1f617fa054b769f861bddd7b8f8a20ac921d11fa
Ken-wangari <wangariken50@gmail.com>
2024-04-21 03:21:52 -0700

    web_flask

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,22 +1,25 @@
+#!/usr/bin/python3
+""" Starts a Flash Web Application C is FUN"""
 from flask import Flask
-
 app = Flask(__name__)
 
+
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
     """ Prints a Message when / is called """
     return 'Hello HBNB!'
 
+
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ Prints a Message when /hbnb is called """
     return 'HBNB'
 
+
+@app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
     """ Prints a Message when /c is called """
     return "C " + text.replace('_', ' ')
-
-app.add_url_rule('/', 'hello_hbnb', hello_hbnb)
-app.add_url_rule('/hbnb', 'hbnb', hbnb)
-app.add_url_rule('/c/<text>', 'c_is_fun', c_is_fun)
 
 if __name__ == "__main__":
     """ Main Function """
