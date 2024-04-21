3dbc360172c96f86edda9eaf6ebc0b937ea8b762
Ken-wangari <wangariken50@gmail.com>
2024-04-21 03:19:04 -0700

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,19 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
-def hello_route():
-    """
-    Displays 'Hello HBNB!'
-    Returns:
-        str: "Hello HBNB"
-    """
-    return "Hello HBNB!"
-
-
-app.add_url_rule('/', 'hello_route', hello_route)
-
+@app.route('/', methods=['GET'], strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
 
