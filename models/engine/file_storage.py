f735c1c686453a9a1fe226a41065d5b27a15e9c7
Ken-wangari <wangariken50@gmail.com>
2024-04-22 11:02:27 -0700

    web_flask

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,86 +1,71 @@
 #!/usr/bin/python3
-"""This is the file storage class for AirBnB"""
+"""Defines the FileStorage class."""
 import json
 from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
 from models.amenity import Amenity
+from models.city import City
 from models.place import Place
 from models.review import Review
-import shlex
+from models.state import State
+from models.user import User
 
 
 class FileStorage:
-    def __init__(self):
-        self.__file_path = "file.json"
-        self.__objects = {}
+    """Represent an abstracted storage engine.
+
+    Attributes:
+        __file_path (str): The name of the file to save objects to.
+        __objects (dict): A dictionary of instantiated objects.
+    """
+
+    __file_path = "file.json"
+    __objects = {}
 
     def all(self, cls=None):
-        if cls:
-            return {key: obj for key, obj in self.__objects.items() if isinstance(obj, cls)}
-        else:
-            return self.__objects
+        """Return a dictionary of instantiated objects in __objects.
+
+        If a cls is specified, returns a dictionary of objects of that type.
+        Otherwise, returns the __objects dictionary.
+        """
+        if cls is not None:
+            if type(cls) == str:
+                cls = eval(cls)
+            cls_dict = {}
+            for k, v in self.__objects.items():
+                if type(v) == cls:
+                    cls_dict[k] = v
+            return cls_dict
+        return self.__objects
 
     def new(self, obj):
-        key = "{}.{}".format(type(obj).__name__, obj.id)
-        self.__objects[key] = obj
+        """Set in __objects obj with key <obj_class_name>.id."""
+        self.__objects["{}.{}".format(type(obj).__name__, obj.id)] = obj
 
     def save(self):
-        with open(self.__file_path, 'w', encoding="UTF-8") as f:
-            json.dump({key: obj.to_dict() for key, obj in self.__objects.items()}, f)
+        """Serialize __objects to the JSON file __file_path."""
+        odict = {o: self.__objects[o].to_dict() for o in self.__objects.keys()}
+        with open(self.__file_path, "w", encoding="utf-8") as f:
+            json.dump(odict, f)
 
     def reload(self):
+        """Deserialize the JSON file __file_path to __objects, if it exists."""
         try:
-            with open(self.__file_path, 'r', encoding="UTF-8") as f:
-                data = json.load(f)
-                for key, value in data.items():
-                    class_name = value['__class__']
-                    cls = globals()[class_name]
-                    self.__objects[key] = cls(**value)
+            with open(self.__file_path, "r", encoding="utf-8") as f:
+                for o in json.load(f).values():
+                    name = o["__class__"]
+                    del o["__class__"]
+                    self.new(eval(name)(**o))
         except FileNotFoundError:
             pass
 
     def delete(self, obj=None):
-        if obj:
-            key = "{}.{}".format(type(obj).__name__, obj.id)
-            del self.__objects[key]
+        """Delete a given object from __objects, if it exists."""
+        try:
+            del self.__objects["{}.{}".format(type(obj).__name__, obj.id)]
+        except (AttributeError, KeyError):
+            pass
 
     def close(self):
+        """Call the reload method."""
         self.reload()
-
-class BaseModel:
-    def __init__(self, **kwargs):
-        self.id = kwargs.get('id', None)
-
-    def to_dict(self):
-        return {'__class__': type(self).__name__, 'id': self.id}
-
-class User(BaseModel):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-class State(BaseModel):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-class City(BaseModel):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-class Amenity(BaseModel):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-class Place(BaseModel):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-class Review(BaseModel):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-if __name__ == "__main__":
-    storage = FileStorage()
-    storage.reload()
 
